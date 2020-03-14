05f3ab559cd45a213b554d70b7f9971dffde80c0
Kevin Oliveira <me@kevinoliveira.com.br>
2020-03-14 04:38:14 -0300

    Add splashscreen

--- header.php
+++ header.php
@@ -11,14 +11,17 @@
   <link rel="dns-prefetch" href="//google-analytics.com">
   <link rel="dns-prefetch" href="//www.google-analytics.com">
 
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://use.typekit.net/fyd2qsq.css">
+
   <!-- Favicon -->
-  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/public/assets/img/favicon/apple-touch-icon.png'; ?>">
-  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . '/public/assets/img/favicon/favicon-32x32.png'; ?>">
-  <link rel="icon" type="image/png" sizes="194x194" href="<?php echo get_template_directory_uri() . '/public/assets/img/favicon/favicon-194x194.png'; ?>">
-  <link rel="icon" type="image/png" sizes="192x192" href="<?php echo get_template_directory_uri() . '/public/assets/img/favicon/android-chrome-192x192.png'; ?>">
-  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . '/public/assets/img/favicon/favicon-16x16.pn'; ?>g">
-  <link rel="manifest" href="<?php echo get_template_directory_uri() . '/public/assets/img/favicon/site.webmanifest'; ?>">
-  <link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/public/assets/img/favicon/safari-pinned-tab.svg'; ?>" color="#000000">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/public/img/favicon/apple-touch-icon.png'; ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . '/public/img/favicon/favicon-32x32.png'; ?>">
+  <link rel="icon" type="image/png" sizes="194x194" href="<?php echo get_template_directory_uri() . '/public/img/favicon/favicon-194x194.png'; ?>">
+  <link rel="icon" type="image/png" sizes="192x192" href="<?php echo get_template_directory_uri() . '/public/img/favicon/android-chrome-192x192.png'; ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . '/public/img/favicon/favicon-16x16.pn'; ?>g">
+  <link rel="manifest" href="<?php echo get_template_directory_uri() . '/public/img/favicon/site.webmanifest'; ?>">
+  <link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/public/img/favicon/safari-pinned-tab.svg'; ?>" color="#000000">
   <meta name="msapplication-TileColor" content="#000000">
   <meta name="theme-color" content="#000000">
 
--- src/js/components/loaders/LoaderSplashscreen.vue
+++ src/js/components/loaders/LoaderSplashscreen.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="splashscreen"></div>
+  <transition name="fade">
+    <div v-if="show" class="loader--overlay loader-splashscreen">
+      <i class="icon-logo"></i>
+      <h1 class="loader-splashscreen__title">
+        <vue-typer :repeat="0" :pre-type-delay="1500" text="Ouse."></vue-typer
+        ><br />
+        <vue-typer
+          :repeat="0"
+          :pre-type-delay="1800"
+          @completed="typed"
+          text="Salte para uma nova vida."
+        ></vue-typer>
+      </h1>
+    </div>
+  </transition>
 </template>
 
 <script>
+import { VueTyper } from "vue-typer";
+
 export default {
   data() {
     return {
-      show: true
+      show: localStorage.getItem("viewIntro") ? false : true,
+      slogan: false
     };
   },
   mounted() {
@@ -21,12 +38,21 @@ export default {
       }
 
       return $body.classList.remove("overflow-hidden");
+    },
+    typed() {
+      setTimeout(() => {
+        localStorage.setItem("viewIntro", true);
+        this.show = false;
+      }, 500);
     }
   },
   watch: {
     show() {
       this.lockOverlow();
     }
+  },
+  components: {
+    VueTyper
   }
 };
 </script>
