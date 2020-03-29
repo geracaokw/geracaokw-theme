6a8dcee9d727fd8a3541923a59d1ea87a59ed1c7
Kevin Oliveira <me@kevinoliveira.com.br>
2020-03-29 01:28:13 -0300

    Add post content

--- views/template-page.php
+++ views/template-page.php
@@ -6,6 +6,37 @@
 <Navbar></Navbar>
 
 <main class="main" role="main">
+  <article class="post">
+    <header class="post-header">
+      <figure class="post-cover">
+        <picture>
+          <source media="(min-width: )" srcset="">
+          <source media="(min-width: )" srcset="">
+          <img src="#" alt="">
+        </picture>
+        <figcaption class="post-cover-caption">
+          <h1 class="post-cover__title">Nós somos a Geração Karol Wojtyla</h1>
+          <div class="post-cover__text">
+            <p>Saiba de onde viemos (pó), para onde voltaremos (pó) e qual a nossa missão nesta tera.</p>
+          </div>
+        </figcaption>
+      </figure>
+    </header>
+
+    <div class="post-content">
+      <blockquote><p>Jovens de todo o mundo tenham a santa ousadia de serem os santos do novo milênio</p></blockquote>
+      <h3>E desde que conhecemos essa frase, dita por São João  Paulo II na sua vinda ao Brasil em 2000, esse desejo de ser santo e jovem arde em nosso coração.</h3>
+      <p>Esta é a frase que nos guia nos problemas do dia-a-dia, nos dias de tristeza e que achamos que nada dará certo. Esta frase de São João Paulo II, nosso padroeiro, é o que nos dá esperança de que vale a pena uma vida com Jesus, de que vale a pena se consumir pela juventude e pelas coisas de Deus.</p>
+      <p>A Geração Karol Wojtyla, nasceu em 2013 como <strong>Predestinados à Ousadia de Cristo</strong>, na Paróquia São Geraldo Magella desde sempre com o carisma da ousadia. Ousadia de estar no mundo mas não ser do mundo, ousadia de evangelizar jovens da nossa região e ousadia de querer largar os desejos carnais para viver uma vida com Deus.</p>
+      <h3 class="post-hero">Ousadia&<br>
+      Alegria&<br>
+      Perseverança&<br>
+      Obediência.</h3>
+      <p>No dia 22/10/2018 depois de muita oração sentimos no coração o chamado de ser uma geração de jovens que busca algo mais, com ousadia, alegria, perseverança e obediência nós, que já gostávamos muito de tudo que São João Paulo II fez, sentimos no coração de ser a geração de jovens que dá continuidade na missão de Karol Wojtyla. Não porque nos achamos superiores ou os mais devotos de São João Paulo II, mas porque somos homens e mulheres, meninos e meninas que falham, que caem, mas que se levantam com ousadia para dar um passo mais, em direção a Jesus.</p>
+      <p>Queremos ser o rosto e memória de São João Paulo II nesta terra.</p>
+      <p>Com prazer, da Geração Karol Wojtyla!</p>
+    </div>
+  </article>
 </main>
 
 <?php get_footer(); ?>
